1b8c41e5f7da288fb5700725ff27d400e869d1d6
FanHuanQing <huanqing19901001@163.com>
2020-08-21 21:07:02 +0800

    Add content about 'hashCode()'

--- utils/Emp.java
+++ utils/Emp.java
@@ -54,5 +54,42 @@ public class Emp {
 	public String toString() {
 		return "Emp [name=" + name + ", age=" + age + ", gender=" + gender + ", salary=" + salary + "]";
 	}
+
+	// ÖØÐ´hashCode()·½·¨
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + gender;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Emp other = (Emp) obj;
+		if (age != other.age)
+			return false;
+		if (gender != other.gender)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+			return false;
+		return true;
+	}
 	
 }

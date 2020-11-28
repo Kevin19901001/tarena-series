cf3020e6fa27260883b6cd276a8dfa616fa7a1e0
huanqing19901001 <huanqing19901001@163.com>
2020-11-28 22:40:48 +0800

    练习：使用FOS和FIS复制文件

--- chapter04/Case02Excercise.java
+++ chapter04/Case02Excercise.java
@@ -63,5 +63,36 @@ public class Case02Excercise {
 	
 	
 	// 3. 实现文件复制
+	// 使用FileOutputStream类和FileInputStream类实现文件复制。
+	// 1. 使用FileOutputStream类的write(int d)方法和FileInputStream类的read()方法复制fos.dat文件为fos_copy1.dat文件。
+	// 2. 使用FileOutputStream类的write(byte[] b)和FileInputStream类的read(byte[] b)方法复制fos.dat为fos_copy2.dat文件。
+	@Test
+	public void testCopy1() throws Exception {
+		FileInputStream fis = new FileInputStream("fos.dat");
+		FileOutputStream fos = new FileOutputStream("fos_copy1.dat");
+		
+		int d = -1;
+		while((d = fis.read()) != -1) {
+			fos.write(d);
+		}
+		
+		fis.close();
+		fos.close();
+	}
 	
+	@Test
+	public void testCopy2() throws Exception {
+		FileInputStream fis = new FileInputStream("fos.dat");
+		FileOutputStream fos = new FileOutputStream("fos_copy2.dat");
+		
+		int length = -1;
+		byte[] b = new byte[32];
+		while((length = fis.read(b)) != -1) {
+			fos.write(b, 0, length);
+		}
+		
+		System.out.println("复制完毕！");
+		fis.close();
+		fos.close();
+	}
 }
